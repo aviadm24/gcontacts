80a569804347c5ae49d5be2e635ebd8882a6a57e
yosef moshe <yosef moshe>
2019-11-04 12:23:47 +0200

    tak awat uniqe in etag and refresh token

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User_tokens(models.Model):
     name = models.CharField(max_length=100)
     crmuserid = models.CharField(max_length=20, unique=True)  #, unique=True
-    refresh_token = models.CharField(max_length=1000, unique=True)
+    refresh_token = models.CharField(max_length=1000)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     state_key = models.CharField(max_length=100)
@@ -16,7 +16,7 @@ class User_tokens(models.Model):
 class User_resourceNames(models.Model):
     user = models.ForeignKey(User_tokens, on_delete=models.CASCADE)
     resource_name = models.CharField(max_length=1000, unique=True)
-    etag = models.CharField(max_length=1000, unique=True)
+    etag = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.resource_name
