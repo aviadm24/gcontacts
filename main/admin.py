5e47b3d849b917163cd6ef21505df986c0dfb489
unknown <aviadm24@gmail.com>
2021-01-04 01:57:12 +0200

    good commit but gitignore not working

---
+++ main/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import User_tokens, User_resourceNames
+
+admin.site.register(User_tokens)
+admin.site.register(User_resourceNames)
---
+++ main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# https://stackoverflow.com/questions/15454008/how-to-reset-db-in-django-i-get-a-command-reset-not-found-error
+class User_tokens(models.Model):
+    name = models.CharField(max_length=100)
+    crmuserid = models.CharField(max_length=20, unique=True)  #, unique=True
+    refresh_token = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    state_key = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.crmuserid
+
+
+class User_resourceNames(models.Model):
+    user = models.ForeignKey(User_tokens, on_delete=models.CASCADE)
+    resource_name = models.CharField(max_length=1000, unique=True)
+    etag = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.resource_name
